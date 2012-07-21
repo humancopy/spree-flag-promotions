d6f29cced993daf77532314e9efa06a1d9f1b043
Mez <meseret.gebre@gmail.com>
2012-07-21 12:31:08 -0400

    updated promoted_items_controller to define new and edit actions

--- app/controllers/spree/admin/promoted_items_controller.rb
+++ app/controllers/spree/admin/promoted_items_controller.rb
@@ -1,5 +1,13 @@
 # flag_promotions/app/controllers/admin/promoted_items_controller.rb
 class Spree::Admin::PromotedItemsController < Spree::Admin::ResourceController
+  def new
+    @promoted_item = @object
+  end
+
+  def edit
+    @promoted_item = @object
+  end
+
   def update_positions
     params[:positions].each do |id, index|
       PromotedItem.update_all(['position=?', index], ['id=?', id])
